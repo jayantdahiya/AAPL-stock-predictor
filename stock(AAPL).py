131d3de1820dae981ac59ffe381b1d64f911d2ee
jayantdahiya <jayantdahiya1999@gmail.com>
2021-01-01 19:37:19 +0530

    Updated main python code

--- stock(AAPL).py
+++ stock(AAPL).py
@@ -1,49 +1,28 @@
-import csv
-import numpy as np
-from sklearn.svm import SVR
+import pandas as pd
 import matplotlib.pyplot as plt
+import quandl
+from sklearn.linear_model import LinearRegression
 
+quandl.ApiConfig.api_key = 'wPaa4PYkmNxA45kLj7f4'
+stock_data = quandl.get('EOD/AAPL', start_date='2013-09-03', end_date='2017-12-28')
+# print(stock_data)
+dataset = pd.DataFrame(stock_data)
+dataset.head()
+dataset.to_csv('AAPL_stock.csv')
 
-plt.switch_backend('template')  
+x = dataset.loc[:,'High':'Adj_Volume']
+y = dataset.loc[:,'Open']
 
+import sklearn.model_selection as model_selection
+x_train,x_test,y_train,y_test = model_selection.train_test_split(x,y,test_size= 0.1,random_state = 0)
+LR = LinearRegression()
+LR.fit(x_train,y_train)
+LR.score(x_test,y_test)
 
+test_data = x.head(1)
+prediction = LR.predict(test_data)
+print('Predicted stock price:')
+print(prediction)
+print('Actual stock price:')
+y.head(1)
 
-dates = []
-prices = []
-
-def get_data(filename):
-	with open(filename, 'r') as csvfile:
-		csvFileReader = csv.reader(csvfile)
-		next(csvFileReader)	# skipping column names
-		for row in csvFileReader:
-			dates.append(int(row[0].split('-')[0]))
-			prices.append(float(row[1]))
-	return
-
-def predict_price(dates, prices, x):
-	dates = np.reshape(dates,(len(dates), 1)) # converting to matrix of n X 1
-
-	svr_lin = SVR(kernel= 'linear', C= 1e3)
-	svr_poly = SVR(kernel= 'poly', C= 1e3, degree= 2)
-	svr_rbf = SVR(kernel= 'rbf', C= 1e3, gamma= 0.1) # defining the support vector regression models
-	svr_rbf.fit(dates, prices) # fitting the data points in the models
-	svr_lin.fit(dates, prices)
-	svr_poly.fit(dates, prices)
-
-	plt.scatter(dates, prices, color= 'black', label= 'Data') # plotting the initial datapoints 
-	plt.plot(dates, svr_rbf.predict(dates), color= 'red', label= 'RBF model') # plotting the line made by the RBF kernel
-	plt.plot(dates,svr_lin.predict(dates), color= 'green', label= 'Linear model') # plotting the line made by linear kernel
-	plt.plot(dates,svr_poly.predict(dates), color= 'blue', label= 'Polynomial model') # plotting the line made by polynomial kernel
-	plt.xlabel('Date')
-	plt.ylabel('Price')
-	plt.title('Support Vector Regression')
-	plt.legend()
-	plt.show()
-
-	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
-
-get_data('AAPL.csv') # calling get_data method by passing the csv file to it
-#print "Dates- ", dates
-#print "Prices- ", prices
-
-predicted_price = predict_price(dates, prices, 29)  
